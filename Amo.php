763eb369568ae46e1942b3a22893ca299296102b
Sema-qq <PO-2-06_024@mail.ru>
2017-08-09 09:22:34 +0500

    Update Amo.php

--- Amo.php
+++ Amo.php
@@ -12,11 +12,14 @@ class Amo
 			'current' => 'https://' . $subdomain . '.amocrm.ru/private/api/v2/json/accounts/current',
 			'get_leads' => 'https://' . $subdomain . '.amocrm.ru/private/api/v2/json/leads/list',
 			'set_leads' => 'https://' . $subdomain . '.amocrm.ru/private/api/v2/json/leads/set',
+			'set_contacts' => 'https://' . $subdomain . '.amocrm.ru/private/api/v2/json/contacts/set',
 			'get_contacts' => 'https://' . $subdomain . '.amocrm.ru/private/api/v2/json/contacts/list',
 			'get_links' => 'https://' . $subdomain . '.amocrm.ru/private/api/v2/json/contacts/links',
 			'get_tasks' => 'https://' . $subdomain . '.amocrm.ru/private/api/v2/json/tasks/list',
 			'get_pipelines' => 'https://' . $subdomain . '.amocrm.ru/private/api/v2/json/pipelines/list'
 		);
+		
+		
 		
 		$user = array(
 			'USER_LOGIN' => $login,
@@ -39,9 +42,9 @@ class Amo
 			502 => 'Bad gateway',
 			503 => 'Service unavailable'
 		);
-		
+
 		$ch = curl_init($url);
-		
+
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
@@ -60,7 +63,7 @@ class Amo
 		$res_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 		
 		curl_close($ch);
-		
+
 		if ($res_code != 200 && $res_code != 204) {
 			exit($errors[$res_code]);
 		}
@@ -98,7 +101,41 @@ class Amo
 			'custom_fields' => $custom_fields
 		);
 		
-		$this->request($this->urls['set_leads'], $leads);
+		return $this->request($this->urls['set_leads'], $leads);
+	}
+	
+	public function setContact ($data)
+	{
+		$leads_list = $this->setLead($data);
+
+		$contacts['request']['contacts']['add'][] = array(
+				'name' => $data['fields'][1]['val'],
+				'linked_leads_id' => array($leads_list->response->leads->add[0]->id),
+				'tags' => $data['tags'],
+				'custom_fields' => array(
+						array(
+							'id' => 327731,
+							'values' => array(
+								array(
+									'value' => $data['fields'][2]['val'],
+									'enum' => 'WORK'									
+								)
+							)
+						),
+						array(
+							'id' => 327737,
+							'values' => array(
+								array(
+									'value' => $data['fields'][3]['val'],
+									'enum' => 'SKYPE'
+								)
+							)
+						)
+					)
+		);
+		//var_dump($contacts);
+		$this->request($this->urls['set_contacts'], $contacts);
+		//var_dump($this->request($this->urls['set_contacts'], $contacts));
 	}
 	
 	public function getTask($id)
